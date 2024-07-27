1ed4b9d8745d2a9b0619f384a51e8eab289a0bd3
MohamedElmi10 <144214489+MohamedElmi10@users.noreply.github.com>
2024-07-27 02:19:42 +0200

    theme done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme } from './utils/Themes';
 
+
+const Body = styled.div`
+background-color:${({ theme }) => theme.bg};
+width: 100%;
+height: 100%;
+overflow-x: hidden;
+`
+  ;
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      Hello
+      <ThemeProvider theme={darkTheme}>
+        <Body>portfolio</Body>
+      </ThemeProvider>
+
     </>
   )
 }
